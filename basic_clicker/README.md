6a6822bcfa0cef3b05acb3abdb4d6c238af1d83f
Jeremy <jlautoacapelle@ucsd.edu>
2023-10-18 20:55:30 -0700

    add basic clicker
    adds Arduino code for the clicker module and a README to explain the device and how to use it

---
+++ basic_clicker/README.md
@@ -0,0 +1,19 @@
+Basic Clicker 
+
+Use this device by pushing the button located in the bottom left. Pushing this button will cause the Arduino Nano 33IoT to write a '1' over the serial communication port with the host computer. 
+
+ADD PICTURE
+
+Device Components:
+
+	- 1x Arduino Nano 33IoT (top right) for reading button input and communicating with the host computer.
+
+	- 1x NE555 timer (top left) for switch debouncing.
+
+	- 1x 7-segment display (left) that increments at the falling edge of the switch signal.
+
+	- 1x Blue LED (middle) that is on while the button is being pushed, off when otherwise.
+
+	- 1x Push button (bottom left) for user input.
+
+	- 1x 74LS47N integrated circuit (bottom middle) for controlling the 7-segment display.
---
+++ basic_clicker/basic_clicker.ino
@@ -0,0 +1,84 @@
+#define DATA_BUTTON   3
+#define DECODER_A     8
+#define DECODER_B     7
+#define DECODER_C     11
+#define DECODER_D     10
+
+int count = 0;
+
+void setup() {
+  pinMode(DATA_BUTTON, INPUT);
+  attachInterrupt(DATA_BUTTON, isr_data_button_falling, FALLING);
+
+  pinMode(DECODER_A, OUTPUT);
+  pinMode(DECODER_B, OUTPUT);
+  pinMode(DECODER_C, OUTPUT);
+  pinMode(DECODER_D, OUTPUT);
+    
+  Serial.begin(9600);
+
+  disp(count);
+}
+
+void loop() {
+}
+
+void disp(int num)
+{
+  switch(num)
+  {
+    case 0:
+      seven_seg(0, 0, 0, 0);
+      return;
+    case 1:
+      seven_seg(0, 0, 0, 1);
+      return;
+    case 2: 
+      seven_seg(0, 0, 1, 0);
+      return;
+    case 3:
+      seven_seg(0, 0, 1, 1);
+      return;
+    case 4: 
+      seven_seg(0, 1, 0, 0);
+      return;
+    case 5:
+      seven_seg(0, 1, 0, 1);
+      return;
+    case 6:
+      seven_seg(0, 1, 1, 0);
+      return;
+    case 7:
+      seven_seg(0, 1, 1, 1);
+      return;
+    case 8:
+      seven_seg(1, 0, 0, 0);
+      return;
+    case 9:
+      seven_seg(1, 0, 0, 1);
+      return;
+    default:
+      seven_seg(0, 0, 0, 0);
+      return;
+  }
+}
+
+void seven_seg(bool D, bool C, bool B, bool A)
+{
+  digitalWrite(DECODER_A, A);
+  digitalWrite(DECODER_B, B);
+  digitalWrite(DECODER_C, C);
+  digitalWrite(DECODER_D, D);
+}
+
+void isr_data_button_falling()
+{
+  count++;
+  if(count>9)
+  {
+    count = 0;
+  }
+  disp(count);
+  Serial.println(1);
+  delay(3000);
+}
